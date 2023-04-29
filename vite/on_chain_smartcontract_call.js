532d1d447664ff5957fa46c043bbec0efecc462f
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-04-29 23:05:47 +0530

    Update on_chain_smartcontract_call.js

--- vite/on_chain_smartcontract_call.js
+++ vite/on_chain_smartcontract_call.js
@@ -1,28 +1,57 @@
-import HTTP_RPC from '@vite/vitejs-http');
-import { ViteAPI, accountBlock } from '@vite/vitejs';
+const {accountBlock, ViteAPI, abi} = require('@vite/vitejs');
+const {HTTP_RPC } = require('@vite/vitejs-http')
+const{get_key}=require('./vite_hex.js')
+const viteProvider =new ViteAPI(new HTTP_RPC("https://node-vite.thomiz.dev"), () => {})
+const {BigNumber} = require('bignumber.js');
+const {multiplier}=require('./objects.js')
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-const AccountBlock = accountBlock.AccountBlock;
-const api = new ViteAPI(new HTTP_RPC("http://example.com"), () => {
-    console.log("Connected");
-});
+async function get_difficulty(myAccountBlock)
+{const { difficulty } = await viteProvider.request('ledger_getPoWDifficulty', {
+        address: myAccountBlock.address,
+        previousHash: myAccountBlock.previousHash,
+        blockType: myAccountBlock.blockType,
+        toAddress: myAccountBlock.toAddress,
+        data: myAccountBlock.data
+    });
+    if (difficulty) {
+        // Call GVite-RPC API to calculate nonce from difficulty
+        const getNonceHashBuffer = Buffer.from(myAccountBlock.originalAddress + myAccountBlock.previousHash, 'hex');
+        const getNonceHash = utils.blake2bHex(getNonceHashBuffer, null, 32);
+        const nonce = await viteProvider.request('util_getPoWNonce', difficulty, getNonceHash)
 
+        myAccountBlock.setDifficulty(difficulty);
+        myAccountBlock.setNonce(nonce);
+    }
 
-var result=abi.encodeFunctionCall( ABI of the contract);
+}
+
+async function vitex_send(token_id,amount,n,count){
+    if(count==6)
+    return
+    await sleep(1500)
+    key=await get_key(n)
+    am=amount
+    amount=BigNumber(amount)
+    amount=amount*BigNumber(10**multiplier[token_id])
+    amount=amount.toLocaleString('fullwide', { useGrouping: false }) ;
+result=abi.encodeFunctionCall({
+	'type': 'function', 'name': 'DexFundUserDeposit', 'inputs': [] 
+    });
 var base_64 = Buffer.from(result, 'hex').toString('base64')
-
+const AccountBlock = accountBlock.AccountBlock;
 
 const myAccountBlock = new AccountBlock({
     blockType: 2,
-    address: 'your address',
-    toAddress: 'your toAddress',
-    tokenId: 'your tokenId',
-    amount: 'your amount',
-    data: base_64
+    address:key[1],
+    toAddress:"vite_0000000000000000000000000000000000000006e82b8ba657",
+    tokenId: token_id,
+    amount: amount,
+    data:base_64
 });
-myAccountBlock.setProvider(api).setPrivateKey('your privateKey');
-
-myAccountBlock.autoSend().then(data => {
-    console.log('success', data);
-}).catch(err => {
-    console.warn(err);
-});
+myAccountBlock.setProvider(viteProvider).setPrivateKey(key[0]);
+await myAccountBlock.autoSetPreviousAccountBlock().catch(()=>{ return vitex_send(token_id,am,n,count+1)});
+ await get_difficulty(myAccountBlock).catch(()=>{ return vitex_send(token_id,am,n,count+1)});
+await myAccountBlock.autoSend().catch((err)=>{return vitex_send(token_id,am,n,count+1)})}
